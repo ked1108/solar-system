33a05819f92a13a1dd288d7df5e482d409aa1328
Kedar Shinde <kedarshinde8011@gmail.com>
2024-07-13 18:27:13 +0530

    Scaling is causing some issues and weird behaviour needs further investigation

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 #include <json/json.h>
 #include <fstream>
+#include <cmath>
 
 #include <iostream>
 
@@ -12,7 +13,7 @@ const int screenHeight = 720;
 
 std::vector<Planet> planets;
 std::shared_ptr<Body> Sun(new Body("Sun", (Vector3) {0, 0, 0},
-                                   109 * SCALE, YELLOW, 333000));
+                                   30 * SCALE*pow(10,8), YELLOW, 333000));
 
 void loadPlanets();
 
@@ -21,7 +22,7 @@ int main() {
     SetTargetFPS(60);
 
     Camera3D camera = { 0 };
-    camera.position = (Vector3){ -7.0f, 5.0f, -7.0f }; // Camera position
+    camera.position = (Vector3){ static_cast<float>(30.0f*SCALE*pow(10,8)+10.0f), 5.0f, static_cast<float>(30.0f*SCALE*pow(10,8) + 10.0f) }; // Camera position
     camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };      // Camera looking at point
     camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
     camera.fovy = 45.0f;                                // Camera field-of-view Y
@@ -29,7 +30,7 @@ int main() {
     DisableCursor();
 
     loadPlanets();
-
+    int p = 0;
     while (!WindowShouldClose())        // Detect window close button or ESC key
     {
         // Update
@@ -37,8 +38,15 @@ int main() {
         UpdateCamera(&camera, CAMERA_FREE);
 
 
-        if (IsKeyPressed('Z')) camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
+        if (IsKeyPressed('Z')){
+            camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
+        }
+        if (IsKeyPressed('N')) {
+            camera.target = planets[p].get_pos();
+            camera.position = {planets[p].get_radius()+10.0f, 5.0f, planets[p].get_radius() + 10.0f};
+            p = ((p + 1)%8 + 8)%8;
 
+        }
 
         //----------------------------------------------------------------------------------
 
@@ -55,7 +63,7 @@ int main() {
         // std::cout << Sun->get_radius() <<", " << ColorToInt(Sun->get_color()) << std::endl;
         DrawSphere(Sun->get_pos(), Sun->get_radius(), Sun->get_color());
 
-        DrawGrid(10, 1.0f);
+        DrawGrid(10000, 50);
 
         for(Planet planet: planets)
             DrawSphere(planet.get_pos(), planet.get_radius(), planet.get_color());
@@ -92,8 +100,8 @@ void loadPlanets() {
 
     for(Json::Value::ArrayIndex i = 0; i != planets_root.size(); i++) {
         planets.emplace_back(planets_root[i]["name"].asString(),
-                             (Vector3){static_cast<float>(planets_root[i]["semi_major_axis"].asFloat()), 0, 0},
-                             planets_root[i]["equatorial_diameter"].asFloat()/2.0f,
+                             (Vector3){static_cast<float>(planets_root[i]["semi_major_axis"].asFloat()*AU), 0, 0},
+                             planets_root[i]["equatorial_diameter"].asFloat()/2.0f*SCALE*pow(10,9),
                              RED,
                              planets_root[i]["mass"].asFloat(),
                              Sun);
